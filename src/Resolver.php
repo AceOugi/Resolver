fb707ca9615abe385abd94173d363c8e8ed5b62d
Assouan Dinari <git@aceougi.com>
2016-05-08 19:34:56 +0200

    Add throw InvalidArgumentException

--- src/Resolver.php
+++ src/Resolver.php
@@ -6,26 +6,23 @@ class Resolver
 {
     /**
      * Cast predisposed callable to callable
+     *
      * @param callable $callable
+     * @throws \InvalidArgumentException
      * @return callable
      */
     public static function resolve($callable) : callable
     {
-        if (is_callable($callable))
-            return $callable;
-
         if (is_string($callable))
         {
-            // String class ==> 'Namespace\\Class' or Namespace\Class::class
             if (class_exists($callable))
                 return new $callable;
-            // String class.method ==> 'Namespace\\Class.method'
-            if ($class = strstr($callable, '.', true) AND
-                $method = strstr($callable, '.') AND
-                $method = substr($method, 1)
-            )
+
+            if ($class = strstr($callable, '.', true) AND $method = substr(strstr($callable, '.'), 1))
                 return [new $class, $method];
         }
+
+        throw new \InvalidArgumentException();
     }
 
     /**
